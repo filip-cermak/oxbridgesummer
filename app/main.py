f76ab80bf5d8c72ce8f49ab46237a227544995dc
Filip Cermak <filipcermak@pop-os.localdomain>
2023-08-10 12:30:58 +0100

    modifying code

--- app/main.py
+++ app/main.py
@@ -113,8 +113,3 @@ async def search_cv(file: UploadFile = File(...)):
     text = page.extract_text()
     embedding = embed_vectors(str(text))
     return search(embedding)
-
-if __name__ == "__main__":
-    import uvicorn
-    server_port = int(os.environ.get('PORT', 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=server_port, log_level="info")
